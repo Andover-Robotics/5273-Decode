e9bea769475c890202247bbfdfe68e0bca4841f7
tango8 <ethan.b.tang@gmail.com>
2025-10-03 18:54:03 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/roadrunner/AutoPathNavigator.java
@@ -0,0 +1,24 @@
+// Main autonomous code
+// uses RoadRunner
+
+package org.firstinspires.ftc.teamcode.auto;
+
+// Road Runner imports
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.Vector2d;
+
+// Hardware imports
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+// Other
+import org.firstinspires.ftc.teamcode.auto.miscRR.MecanumDrive;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class AutoPathNavigator {
+}
