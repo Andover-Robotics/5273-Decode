f4bad4bef7faaee9098a7d4bea7993b165b90e8c
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-10-07 12:11:50 -0400

    made outtake rpm based

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Outtake {
     private final MotorEx motor;
-    private double power;
+    private double rpm;
+    double lastTime;
 
     public Outtake(HardwareMap hardwareMap)
     {
         motor = new MotorEx(hardwareMap, "outtake");
+        motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        lastTime = System.nanoTime();
     }
 
     public void stop()
@@ -19,19 +23,30 @@ public class Outtake {
 
     public void run()
     {
-        motor.set(power);
+        motor.setVelocity(rpmToTicksPerSecond(rpm));
     }
 
-    public void setPower(double newPower)
+    public void setRPM(double newPower)
     {
-        power = newPower;
-        motor.set(power);
+        rpm = newPower;
     }
 
-    public double getPower()
+    public double getRpm()
     {
-        return power;
+        return rpm;
     }
 
+    public double getRPM()
+    {
+        double ticksPerSecond = motor.getVelocity();
+        double ticksPerRev = motor.getCPR();
+        double revsPerSecond = ticksPerSecond / ticksPerRev;
+        return revsPerSecond * 60.0;
+    }
+
+    public double rpmToTicksPerSecond(double rpm) {
+        double ticksPerRev = motor.getCPR(); // counts per revolution
+        return (rpm / 60.0) * ticksPerRev;
+    }
     //TODO: Make algorithm to determine power needed based on distance
 }
