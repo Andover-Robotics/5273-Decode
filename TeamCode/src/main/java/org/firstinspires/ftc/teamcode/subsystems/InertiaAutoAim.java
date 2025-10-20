3102394a6be4c6e9aa7df82dac7e1c9424dd68aa
Noah Turner <noah.o.turner11@gmail.com>
2025-10-20 16:54:22 -0400

    Added InertiaAutoAim.java and added getYawDegrees() method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/InertiaAutoAim.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+public class InertiaAutoAim {
+    //Given: Magnitude and direction of Velocity, and range and elevation
+    //Get: needed yaw change
+    private final static double[] robotPos = {0,0,0};
+    // double[] --> {x,y,z}
+    public double getYawDegrees(double[] robotVel, double ballSpeed, double robotYawRad, double goalDistance, double goalElevation) {
+        double baseDistance = Math.sqrt(goalDistance * goalDistance - goalElevation * goalElevation);
+        double[] goalPos = {baseDistance * Math.sin(robotYawRad),goalElevation,baseDistance * Math.cos(robotYawRad)};
+
+        double time = (goalDistance / ballSpeed);
+
+        // get the needed yaw change
+        double dx = goalPos[0] - (robotPos[0] + robotVel[0] * time);
+        double dz = goalPos[2] - (robotPos[2] + robotVel[2] * time);
+        double yaw = Math.atan2(dz, dx) - robotYawRad;
+
+        return Math.toDegrees(yaw);
+    }
+}
