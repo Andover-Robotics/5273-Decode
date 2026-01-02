346099770c445f7acb61b59d835f80d62e51aa8f
tango8 <ethan.b.tang@gmail.com>
2026-01-02 16:26:16 -0500

    rpm faster for quickspin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Bot.java
@@ -192,7 +192,7 @@ public class Bot {
     }
 
     private Action actionNonIndexedDump() {
-        final double rpm = getTargetRpm();
+        final double rpm = getTargetRpm() * 1.5;
         return new SequentialAction(
                 new InstantAction(actuator::upQuick),// lower up position for quick dump
                 new InstantAction(() -> outtake.set(rpm)),
