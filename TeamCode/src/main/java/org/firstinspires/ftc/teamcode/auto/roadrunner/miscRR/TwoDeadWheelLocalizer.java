40e102ead31bdb204b98229054848c3a15bc2d95
naushadwaqar <nwaqar970209@gmail.com>
2026-01-23 18:26:14 -0500

    fixed deadwheel stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/roadrunner/miscRR/TwoDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/roadrunner/miscRR/TwoDeadWheelLocalizer.java
@@ -53,8 +53,8 @@ public final class TwoDeadWheelLocalizer implements Localizer {
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
         //todo: make sure its not reading the motor encoder it should be the deadwheel encoder
         // get the DcMotorEx objects first
-        DcMotorEx parMotor = hardwareMap.get(DcMotorEx.class, "par-deadwheel");
         DcMotorEx perpMotor = hardwareMap.get(DcMotorEx.class, "intake");
+        DcMotorEx parMotor = hardwareMap.get(DcMotorEx.class, "perpDeadwheel");
 
         // set them to RUN_WITHOUT_ENCODER so the motor controller just reports ticks
         parMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -66,7 +66,7 @@ public final class TwoDeadWheelLocalizer implements Localizer {
 
 
         // TODO: reverse encoder directions if needed
-        par.setDirection(DcMotorSimple.Direction.REVERSE);
+        par.setDirection(DcMotorSimple.Direction.FORWARD);
         perp.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.imu = imu;
