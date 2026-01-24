f8c8e906bba89189567cb32bb4c8359bb0cbd0c4
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2026-01-24 17:37:14 -0500

    Actuator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Actuator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Actuator.java
@@ -6,14 +6,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
 public class Actuator {
-    public static double DOWN = 0.0;      // flush with the floor of platform
-    public static double UP_QUICK = 0.20; // used for quick (non-indexed) outtake
-    public static double UP_INDEXED = 0.34; // used for indexed outtake
+    public static double DOWN = 0.49;      // flush with the floor of platform
+    public static double UP_QUICK = 0.25; // used for quick (non-indexed) outtake
+    public static double UP_INDEXED = 0.13; // used for indexed outtake
 
     private boolean activated;
 
     private final SimpleServo servo;
-
     private double waitTime = 0.5; // seconds
 
     public Actuator(HardwareMap hardwareMap) {
