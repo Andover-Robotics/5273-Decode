9c3b862b9d2fed2f799147a0cd3cd48b3987ffc8
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2026-01-25 12:06:51 -0500

    No camerascan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/DistanceRegressionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/DistanceRegressionTeleOp.java
@@ -176,7 +176,6 @@ public class DistanceRegressionTeleOp extends LinearOpMode {
         // Begin continuous lock
         if (g2.wasJustPressed(GamepadKeys.Button.X)) {
             continuousAprilTagLock = true;
-            aprilTag.setCurrentCameraScannedId(0);
         }
 
         // Stop continuous lock
