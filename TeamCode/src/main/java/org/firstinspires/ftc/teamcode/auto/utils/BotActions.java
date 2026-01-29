01824d39225e0115801d66b634d77a37c8ff5fc5
tango8 <ethan.b.tang@gmail.com>
2026-01-29 07:21:19 -0500

    tested, color sensing works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/utils/BotActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/utils/BotActions.java
@@ -93,7 +93,7 @@ public class BotActions {
                     telemetry.addData("Rotated To Motif", "Color");
                     // Indexer.IndexerState gotoState = Indexer.IndexerState.values()[(state.index - 1) % Indexer.IndexerState.values().length];
                     Indexer.IndexerState gotoState = state;
-                    indexer.moveTo(gotoState, true);
+                    indexer.moveTo(gotoState);
                     return;
                 }
             }
