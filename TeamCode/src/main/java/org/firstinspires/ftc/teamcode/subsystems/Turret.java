e47862888d1040657ea4ae40a0211e29e0c691e3
naushadwaqar <nwaqar970209@gmail.com>
2026-02-18 16:02:45 -0500

    Reapply "adjust for limelight being on turret"
    This reverts commit 5f573ac16e8665b1babf9b5e361e2d2588ed45c2.
    
    Revert "adjust for limelight being on turret"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -1,19 +1,16 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.teamcode.auto.roadrunner.miscRR.TwoDeadWheelLocalizer;
-import org.firstinspires.ftc.teamcode.subsystems.limelight.AprilTagAimer;
-
+@Config
 public class Turret {
-    private static final double TICKS_TO_DEGREES = 0;
-    private static final double ZERO_OFFSET = 0;
+    public static double TICKS_TO_DEGREES = 0;
+    public static double ZERO_OFFSET = 0;
     private final DcMotor motor;
 
-    public Turret(HardwareMap hardwareMap, IMU imu, TwoDeadWheelLocalizer deadWheelLocalizer) {
+    public Turret(HardwareMap hardwareMap) {
         motor = hardwareMap.get(DcMotor.class, "TurretMotor");
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         // May be needed?
