604ba347c2606b3cda86f68b3d8a11ba61b92507
tango8 <ethan.b.tang@gmail.com>
2026-06-15 08:17:50 -0400

    Red and blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/opmodes/CloseTwelveBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/opmodes/CloseTwelveBlue.java
@@ -20,29 +20,29 @@ import org.firstinspires.ftc.teamcode.auto.utils.BotActions;
 import org.firstinspires.ftc.teamcode.auto.utils.Hardware;
 
 @Config
-@Autonomous(name = "Close Fifteen Ball Red Gate No Intake Auto", group = "Autonomous")
-public class CloseTwelve extends LinearOpMode {
+@Autonomous(name = "Close Fifteen Ball Blue Gate No Intake Auto", group = "Autonomous")
+public class CloseTwelveBlue extends LinearOpMode {
     private Hardware hardware;
     private BotActions botActions;
     private MecanumDrive drive;
 
     //USE SAME LOCALIZATION STYLE AS AIMER (90 degrees faces the goals, 0 degs faces side with red goal, 180 degs faces side with blue goal, +y is towards goals)
-    public static double startX = 113.0;
+    public static double startX = 180 - CloseTwelveRed.startX;
     public static double startY = 129;
     public static double startAngle = Math.toRadians(90);
     // based on blue for now : ill make cross compatible soon
-    public static double intakingAngle = Math.toRadians(0);
+    public static double intakingAngle = Math.toRadians(180);
 
     //row numerations start at 0 for ease
     //array of row y
     public static double[] rowStartY = {81.2, 56.72, 33.0};
-    public static double[] rowStartX = {103.0, 102.0, 94.0};
+    public static double[] rowStartX = {180 - 103.0, 180 - 102.0, 180 - 94.0};
     // array of how far to go forward in each row
     public static double[] rowForwards = {18, 28, 28};
 
     //shoot pos
     public static double shootY = 81.0;
-    public static double shootX = 94.0;
+    public static double shootX = 180 - CloseTwelveRed.shootX;
 
     public static Pose2d startPose = new Pose2d(startX, startY, startAngle);
 
@@ -52,13 +52,13 @@ public class CloseTwelve extends LinearOpMode {
     public static Pose2d rowTwoStart = new Pose2d(rowStartX[2], rowStartY[2], intakingAngle);
 
     //end poses based on rowForwards: +x for red, -x for blue
-    public static Pose2d rowZeroEnd = new Pose2d(rowStartX[0] + rowForwards[0], rowStartY[0], intakingAngle);
-    public static Pose2d rowOneEnd = new Pose2d(rowStartX[1] + rowForwards[1], rowStartY[1], intakingAngle);
-    public static Pose2d rowTwoEnd = new Pose2d(rowStartX[2] + rowForwards[2], rowStartY[2], intakingAngle);
+    public static Pose2d rowZeroEnd = new Pose2d(rowStartX[0] - rowForwards[0], rowStartY[0], intakingAngle);
+    public static Pose2d rowOneEnd = new Pose2d(rowStartX[1] - rowForwards[1], rowStartY[1], intakingAngle);
+    public static Pose2d rowTwoEnd = new Pose2d(rowStartX[2] - rowForwards[2], rowStartY[2], intakingAngle);
 
-    public static Pose2d gatePose = new Pose2d(131.6, 67, Math.toRadians(-90));
-    public static Pose2d shootPos = new Pose2d(shootX, shootY, Math.toRadians(0));
-    public static Pose2d leavePos = new Pose2d(103, 76, Math.toRadians(0));
+    public static Pose2d gatePose = new Pose2d(180 - 131.6, 67, Math.toRadians(-90));
+    public static Pose2d shootPos = new Pose2d(shootX, shootY, Math.toRadians(180));
+    public static Pose2d leavePos = new Pose2d(180 - 103, 76, Math.toRadians(180));
 
     public static double intakeSettle = 0.25;
 
@@ -98,7 +98,7 @@ public class CloseTwelve extends LinearOpMode {
                 .strafeTo(rowTwoEnd.position)
                 .stopAndAdd(botActions.runContinuousIntake())
                 .stopAndAdd(new InstantAction(() -> botActions.startOuttake(botActions.getTargetRPM())))
-                .strafeToSplineHeading(new Vector2d(shootX - 2, shootY + 2), shootPos.heading.log())
+                .strafeToSplineHeading(new Vector2d(shootX + 2, shootY + 2), shootPos.heading.log())
                 .stopAndAdd(botActions.actionOuttake())
                 .strafeToSplineHeading(leavePos.position, leavePos.heading.log());
 
