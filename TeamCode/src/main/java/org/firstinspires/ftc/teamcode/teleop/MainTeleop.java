1a74d13e56d0a441c7e2c02dc31ca844ab005356
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2026-06-17 11:18:44 -0400

    starting localizataion set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleop.java
@@ -11,10 +11,14 @@ import org.firstinspires.ftc.teamcode.auto.roadrunner.miscRR.MecanumDrive;
 @TeleOp(name = "MainTeleOp", group = "AA_main")
 public class MainTeleop extends LinearOpMode {
 
+    public static double startX = 0;
+    public static double startY = 0;
+    public static double startHeading = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, Math.toRadians(0)));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(startX, startY, Math.toRadians(startHeading)));
         Bot bot = new Bot(hardwareMap, telemetry, drive, gamepad1, gamepad2,false);
         bot.teleopInit();
         waitForStart();
